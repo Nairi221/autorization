8d935f246b41f652ed699c264be3f73775cb087e
Наири Айвазя́н <n-airi@hotmail.com>
2021-02-04 15:21:42 +0400

    fix index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./app')
-const port = process.env.PORT || 5000
+const app = require('./app');
+const port = process.env.PORT || 5000;
 app.listen(port,() => console.log(`server has been started on  ${port}`))
 
